1b3c5b528709ec45f5d84d3a1f7fc2d1b579f608
feiniao <2729678545@qq.com>
2026-05-20 22:36:37 +0800

    更新了魔力池多方快

--- kubejs/server_scripts/machine/recipeTransfer.js
+++ kubejs/server_scripts/machine/recipeTransfer.js
@@ -1,6 +1,9 @@
+let $ManaInfusionRecipe = Java.loadClass("vazkii.botania.common.crafting.ManaInfusionRecipe")
+let $RegistryAccess = Java.loadClass("net.minecraft.core.RegistryAccess")
+let $ResourceKey = Java.loadClass("net.minecraft.resources.ResourceKey")
 MBDRecipeTypeEvents.onTransferProxyRecipe("mbd2:modular_ember_alchemy", e => {
     let event = e.event
-    const {recipeType, proxyTypeId, proxyType, proxyRecipeId, proxyRecipe} = event
+    const { recipeType, proxyTypeId, proxyType, proxyRecipeId, proxyRecipe } = event
 
     let inputList = []
     let aspectList = []
@@ -15,7 +18,7 @@ MBDRecipeTypeEvents.onTransferProxyRecipe("mbd2:modular_ember_alchemy", e => {
         let inputs = proxyRecipe.inputs
 
         let output = proxyRecipe.output
-        
+
         inputs.forEach(input => {
             inputList.push(input)
         })
@@ -27,12 +30,55 @@ MBDRecipeTypeEvents.onTransferProxyRecipe("mbd2:modular_ember_alchemy", e => {
         /**@type {Internal.MBDRecipeSchema$MBDRecipeJS} */
         var recipe = recipeType.recipeBuilder().id(proxyRecipeId + "_mbd2")
         recipe.inputItems(tablet)
-        .inputItems(inputList)
-        .chance(0,builder => {
-            builder.inputItems(aspectList)
-        })
-        .outputItems(output)
+            .inputItems(inputList)
+            .chance(0, builder => {
+                builder.inputItems(aspectList)
+            })
+            .outputItems(output)
 
+        event.mbdRecipe = recipe.buildMBDRecipe()
+    }
+})
+MBDRecipeTypeEvents.onTransferProxyRecipe("mbd2:mana_pool", e => {
+    let event = e.event
+    let inputsList = []
+    const { recipeType, proxyTypeId, proxyType, proxyRecipeId, proxyRecipe } = event
+
+    /** @type {Internal.ManaInfusionRecipe}*/
+    let proxy = proxyRecipe
+    if (proxyTypeId == "botania:mana_infusion") {
+
+        /**@type {Internal.ItemStack[]} */
+        let inputs = proxy.getIngredients()
+
+        /**@type {Internal.ItemStack} */
+        let output = proxy.getResultItem(null)
+
+        let mana = proxy.getManaToConsume()
+
+        /**@type {Internal.MBDRecipeSchema$MBDRecipeJS} */
+        let recipe = recipeType.recipeBuilder().id(proxyRecipeId + "_mbd2")
+
+        inputs.forEach(input => {
+            inputsList.push(input)
+        })
+
+        if (proxy.getRecipeCatalyst() != null) {
+            let catalyst = proxy.getRecipeCatalyst().getDisplayedStacks()
+            
+            recipe.inputItems(inputsList).chance(0, builder =>
+                builder.inputItems([catalyst])
+            )
+                .outputItems([output])
+                .inputMana(mana * 0.8)
+        } else {
+            let catalyst = null
+
+            recipe.inputItems(inputsList)
+                .outputItems([output])
+                .inputMana(mana * 0.8)
+        }
+        
         event.mbdRecipe = recipe.buildMBDRecipe()
     }
 })
